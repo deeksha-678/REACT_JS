7291bf61cee4745ed9c07f71cb1b5ea41e0880fb
deeksha-678 <92356796+deeksha-678@users.noreply.github.com>
2021-12-08 15:38:10 +0530

    commit

---
+++ Router/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+import Navbar from './component/Navbar';
+import Router from './component/Router';
+class App extends Component{
+  render(){
+    return <div>
+<Navbar />
+<Router />
+    </div>
+  }
+}
+
+export default App;
