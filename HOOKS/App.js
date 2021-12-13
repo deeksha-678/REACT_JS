dc90e15b68e0ce9568edb3e4a0f58aaf08d2c4c2
deeksha-678 <92356796+deeksha-678@users.noreply.github.com>
2021-12-13 20:32:08 +0530

    commit

---
+++ HOOKS/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Clickhook from "./component/Clickhook";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Clickhook/>
+      </div>
+    );
+  }
+}
+export default App;
