48c53874a11a47a1b4525f4633a29e300fa8d3f5
deeksha-678 <92356796+deeksha-678@users.noreply.github.com>
2021-12-16 14:24:11 +0530

    commit

---
+++ Eventbinging/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Eventbinding from "./component/Eventbinding";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Eventbinding/>
+      </div>
+    );
+  }
+}
+export default App;
