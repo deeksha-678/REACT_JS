59c8a22274c53c0952948036ebbc771bbcccda4b
deeksha-678 <92356796+deeksha-678@users.noreply.github.com>
2021-12-16 15:19:45 +0530

    commit

---
+++ Lifecycle/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Lifecycle from "./component/Lifecycle";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Lifecycle/>
+      </div>
+    );
+  }
+}
+export default App;
